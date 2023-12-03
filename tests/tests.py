ee8a4f54d9382bc217bd6f4425ed8f386d6dce5a
Dawid Kaplon <dawidkaplon2@gmail.com>
2023-12-03 19:55:31 +0100

    Modify tests file; improve data validator tests

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,6 @@ import pytest
 import sqlite3
 from pathlib import Path
 
-import script
 from database import db_manager, db_parser
 
 TEST_DATA = db_parser.DataParser.parse_json("users.json")
@@ -30,7 +29,7 @@ def temporary_db():
     file_path.unlink()
 
 def clear_test_database(cursor):
-    """After each test, delete all records from the database"""
+    """Delete all records from the database if specific test needs that"""
 
     cursor.execute(
         """
@@ -40,12 +39,12 @@ def clear_test_database(cursor):
 
 
 class TestDataValidator:
-    def test_email(self, temporary_db):
+
+    def test_correct_email(self, temporary_db):
         """Check if e-mail that meets criteria has been successfully added into the database"""
 
-        connection, cursor = temporary_db  # Unpack as the temporary_db fixture is a tuple
+        _, cursor = temporary_db  # Unpack as the temporary_db fixture is a tuple
         correct_emails = 0
-        incorrect_emails = 0
 
         for user in TEST_DATA:
             if db_manager.DataHandler.validate_email(user["email"]):
@@ -57,8 +56,6 @@ class TestDataValidator:
                     (user["email"],),
                 )
                 correct_emails += 1
-            else:
-                incorrect_emails += 1
 
         cursor.execute(
             """
@@ -69,20 +66,45 @@ class TestDataValidator:
 
         assert (
             result == correct_emails
-        )  #  Number of rows in db should equal to correct emails
-        
-        assert (
-            ((len(TEST_DATA)) - result) == incorrect_emails
-        )  # Number of rows not added to db should be the same as incorrect emails
+        )  # Number of rows in db should equal to correct emails
 
         clear_test_database(cursor)
 
-    def test_phone_number(self, temporary_db):
+    def test_incorrect_email(self, temporary_db):
+        """Check if incorrect e-mails are not added to the database"""
+
+        _, cursor = temporary_db
+        incorrect_emails = 0
+
+        for user in TEST_DATA:
+            if not db_manager.DataHandler.validate_email(user["email"]):
+                cursor.execute(
+                    """
+                    INSERT INTO users (email)
+                    VALUES (?)
+                    """,
+                    (user["email"],),
+                )
+                incorrect_emails += 1
+
+        cursor.execute(
+            """
+            SELECT COUNT(1) FROM users
+            """
+        )
+        result = cursor.fetchone()[0]
+
+        assert (
+            result == 0
+        )  # No incorrect emails should be added to the database
+
+        clear_test_database(cursor)
+
+    def test_correct_phone_number(self, temporary_db):
         """Check if correct phone-num has been added into the database"""
 
-        connection, cursor = temporary_db
+        _, cursor = temporary_db
         correct_phone_numbers = 0
-        incorrect_phone_numbers = 0
 
         for user in TEST_DATA:
             if db_manager.DataHandler.validate_phone_num(user["telephone_number"]):
@@ -94,8 +116,6 @@ class TestDataValidator:
                     (user["telephone_number"],),
                 )
                 correct_phone_numbers += 1
-            else:
-                incorrect_phone_numbers += 1
 
         cursor.execute(
             """
@@ -106,9 +126,37 @@ class TestDataValidator:
         result = cursor.fetchone()[0]
 
         assert (
-            result == correct_phone_numbers  
-            # Return True if the number of rows in the db is the same as the number of valid phone numbers in the dataset
+            result == correct_phone_numbers
+        )  # Return True if the number of rows in the db is the same as the number of valid phone numbers in the dataset
+
+        clear_test_database(cursor)
+
+    def test_incorrect_phone_number(self, temporary_db):
+        """Check if incorrect phone numbers are not added to the database"""
+
+        _, cursor = temporary_db
+        incorrect_phone_numbers = 0
+
+        for user in TEST_DATA:
+            if not db_manager.DataHandler.validate_phone_num(user["telephone_number"]):
+                cursor.execute(
+                    """
+                    INSERT INTO users (telephone_number)
+                    VALUES (?)
+                    """,
+                    (user["telephone_number"],),
+                )
+                incorrect_phone_numbers += 1
+
+        cursor.execute(
+            """
+            SELECT COUNT(1) FROM users
+            """
         )
+        result = cursor.fetchone()[0]
+
         assert (
-            ((len(TEST_DATA)) - result) == incorrect_phone_numbers
-        )
+            result == 0
+        )  # No incorrect phone numbers should be added to the database
+
+        clear_test_database(cursor)
